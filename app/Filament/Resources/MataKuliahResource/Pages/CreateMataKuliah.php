011ccb1e607a74f3990ee8a9f6a312d00a375808
shintamaulida273 <0110124201@student.nurulfikri.ac.id>
2025-06-29 20:13:39 +0700

    membuat kegiatan dan menambahkan mata kuliah

--- app/Filament/Resources/MataKuliahResource/Pages/CreateMataKuliah.php
+++ app/Filament/Resources/MataKuliahResource/Pages/CreateMataKuliah.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMataKuliah extends CreateRecord
 {
     protected static string $resource = MataKuliahResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl();
+    }
 }
