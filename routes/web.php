822a497d532550cb083037788cfc35395ad88286
imtiyaayy <imtiyazmufidah08@gmail.com>
2025-07-12 19:02:45 +0700

    Update Imtiyaz IV

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return redirect('/admin'); // <-- UBAH BAGIAN INI
+    return redirect('/user'); // <-- UBAH BAGIAN INI
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
